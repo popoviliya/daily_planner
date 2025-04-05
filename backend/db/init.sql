1d9e41d46c3679273c8daab1af877371da9531b7
popoviliya <popoviliya2017+vocoewrs@yandex.ru>
2025-04-05 22:58:09 +0300

    Updated db structure

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL NOT NULL UNIQUE PRIMARY KEY,
-    login VARCHAR(330) NOT NULL UNIQUE,
+    username VARCHAR(330) NOT NULL UNIQUE,
     password_hash VARCHAR(100) NOT NULL,
     is_active BOOLEAN NOT NULL DEFAULT TRUE,
     first_name VARCHAR(100) NOT NULL,
     middle_name VARCHAR(100),
     last_name VARCHAR(100) NOT NULL,
-    telegram VARCHAR(100) UNIQUE,
     phone VARCHAR(100) UNIQUE,
+    telegram VARCHAR(100) UNIQUE,
     registration_date DATE NOT NULL DEFAULT CURRENT_DATE,
     instructor INTEGER NOT NULL,
     is_instructor BOOLEAN NOT NULL DEFAULT FALSE,
@@ -15,14 +15,14 @@ CREATE TABLE IF NOT EXISTS users (
     instructor_working_hours VARCHAR(100)
 );
 COMMENT ON COLUMN users.id IS 'unique numeric id for each user';
-COMMENT ON COLUMN users.login IS 'users`s email address';
+COMMENT ON COLUMN users.username IS 'users`s email address';
 COMMENT ON COLUMN users.password_hash IS 'hashed and salted users`s password';
 COMMENT ON COLUMN users.is_active IS 'is user active?';
 COMMENT ON COLUMN users.first_name IS 'users`s first name';
 COMMENT ON COLUMN users.middle_name IS 'users`s middle name';
 COMMENT ON COLUMN users.last_name IS 'users`s last name';
-COMMENT ON COLUMN users.telegram IS 'users`s telegram login';
 COMMENT ON COLUMN users.phone IS 'users`s phone number';
+COMMENT ON COLUMN users.telegram IS 'users`s telegram login';
 COMMENT ON COLUMN users.registration_date IS 'date of user`s registration';
 COMMENT ON COLUMN users.instructor IS 'users`s instructor id';
 COMMENT ON COLUMN users.is_instructor IS 'is user a instructor (admin)?';
